c24e2b918a9d44d0ae973b82bb58f93c70f5f826
Christian Dominguez <christian@redbonzai.com>
2023-12-29 01:14:01 -0600

    nx-dev: removed the damn node_modules directories in the services.

---
+++ apps/auth/src/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
+import { UserDocument } from './users/models/user.schema';
+import { TokenPayload } from './interfaces/token-payload.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async login(user: UserDocument, response: Response) {
+    const tokenPayload: TokenPayload = {
+      userId: user._id.toHexString(),
+    };
+
+    const expires = new Date();
+    expires.setSeconds(
+      expires.getSeconds() + this.configService.get('JWT_EXPIRATION'),
+    );
+
+    const token = this.jwtService.sign(tokenPayload);
+    // const token = this.jwtService.sign(tokenPayload, {
+    //   secret: this.configService.get('JWT_SECRET'),
+    //   expiresIn: this.configService.get('JWT_EXPIRATION'),
+    // });
+    //
+
+    response.cookie('Authentication', token, {
+      httpOnly: true,
+      expires,
+    });
+
+    return token;
+  }
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
