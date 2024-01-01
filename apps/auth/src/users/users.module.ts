872bc2d7e01b7f5f4a39944cdc991e85f91feafd
Christian Dominguez <christian@redbonzai.com>
2024-01-01 10:56:27 -0600

    committing uncommitted files

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule, ResponseInterceptor } from '@app/common';
+import { DatabaseModule } from '@app/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UserDocument, UserSchema } from '@app/common';
 import { UsersRepository } from './users.repository';
-// import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -14,14 +13,7 @@ import { UsersRepository } from './users.repository';
     ]),
   ],
   controllers: [UsersController],
-  providers: [
-    // {
-    //   provide: APP_INTERCEPTOR,
-    //   useClass: ResponseInterceptor,
-    // },
-    UsersService,
-    UsersRepository,
-  ],
+  providers: [UsersService, UsersRepository],
   exports: [UsersService],
 })
 export class UsersModule {}
